cea178426881456904e3ed9662ad794c3fb0bd28
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2017-12-26 17:57:58 -0600

    GIP.00.00 17-12-26 Created class

---
+++ src/INIHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Platform - INIHandler
+ * @copyright (C) 2017 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package Platform
+ */
+
+namespace GIndie\Platform;
+
+/**
+ * Description of INIHandler
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @version GIP.00.00 17-12-26 Created class
+ */
+class INIHandler
+{
+    //put your code here
+}
